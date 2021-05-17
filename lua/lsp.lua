da7b73b136ab741c6956bfbaffac46a0f4f05bfa
Gianluca Recchia <gianluca.recchia@protonmail.com>
2021-05-17 12:43:07 +0200

    Customize rust-analyzer

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -24,7 +24,22 @@ nvim_lsp.gopls.setup {
 
 nvim_lsp.rust_analyzer.setup {
 	capabilities = capabilities,
-	on_attach = init_lsp_buffer
+	on_attach = init_lsp_buffer,
+	settings = {
+		['rust-analyzer'] = {
+			assist = {
+				importMergeBehavior = 'last',
+				importPrefix = 'by_self',
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			lens = {
+				debug = false,
+				enable = false,
+			}
+		}
+	}
 }
 
 nvim_lsp.terraformls.setup {

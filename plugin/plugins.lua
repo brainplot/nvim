874579cb5c53d91be9eb7b528097d198e14ab1c4
Gianluca Recchia <gianluca.recchia@protonmail.com>
2021-11-01 20:31:44 +0100

    Use `github-theme` in the `dark_default` variant

--- plugin/plugins.lua
+++ plugin/plugins.lua
@@ -39,7 +39,9 @@ return require('packer').startup(function()
 	use {
 		'projekt0n/github-nvim-theme',
 		config = function()
-			require('github-theme').setup()
+			require('github-theme').setup({
+				theme_style = 'dark_default'
+			})
 		end,
 	}
 

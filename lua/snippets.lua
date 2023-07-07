8de780f4e9380c6118e7060ac2ffcf53e102bea4
Gianluca Recchia <gianluca.recchia@protonmail.com>
2023-07-07 20:40:18 +0200

    Add markdown snippets

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -88,3 +88,13 @@ ls.add_snippets("go", {
 		t({ "", "}" }),
 	}),
 })
+
+ls.add_snippets("markdown", {
+	s({ trig = "img", name = "image" }, {
+		t("!["),
+		i(1),
+		t("]("),
+		i(2),
+		t(")"),
+	}),
+})
